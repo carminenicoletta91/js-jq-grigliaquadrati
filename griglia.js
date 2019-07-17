eb2bf8c702c49b0c412dddbd26318298838b4af6
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-17 18:06:54 +0200

    aggiunta funzione riempimento colore quadrati al click su un quadrato rosso

--- griglia.js
+++ griglia.js
@@ -1,3 +1,10 @@
 // Creare una griglia formata da 8x8 quadratini tutti bianchi.
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati
 // diventano rossi, gli altri diventano verdi
+$( document ).ready(function() {
+  $(".box-quadrato#red").click(function(){
+    $(".box-quadrato#red").addClass("red");
+    $(".box-quadrato#green").addClass("green");
+  });
+
+});
