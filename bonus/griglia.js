42b7db0e403d9118bd1a847069ddd121820ec649
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-18 10:12:33 +0200

    --modificati gli id con delle classi--

--- bonus/griglia.js
+++ bonus/griglia.js
@@ -4,14 +4,14 @@
 $( document ).ready(function() {
   var contatoreverde=0;var contatorerosso =0;
   // funzione per contare i quadrati verdi
-  $(".box-quadrato#green").click(function(){
-    $(this).addClass("green")
+  $(".box-quadrato.green").click(function(){
+    $(this).addClass("verde")
     contatoreverde +=1;
     $("#verdeconteggio").text(contatoreverde)
   });
   // funzione per contare i quadrati rossi
-  $(".box-quadrato#red").click(function(){
-    $(this).addClass("red")
+  $(".box-quadrato.red").click(function(){
+    $(this).addClass("rosso")
     contatorerosso +=1;
     $("#rossoconteggio").text(contatorerosso)
   });
